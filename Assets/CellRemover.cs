bc8e353523a809d7a2b33e51c2b6098347568c46
oscarlegetth <oscar.legetth@gmail.com>
2017-04-28 15:02:32 +0200

    Added some paddles that can select and remove cells, or just remove cells directly

---
+++ Assets/CellRemover.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellRemover : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		other.attachedRigidbody.useGravity = true;
+		// other.attachedRigidbody.isKinematic = true;
+		other.isTrigger = false;
+	}
+}
---
+++ Assets/CellSelector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellSelector : MonoBehaviour {
+
+	ArrayList selectedCells = new ArrayList();
+
+	void OnTriggerEnter(Collider other) {
+		other.GetComponent<Renderer> ().material.color = Color.red;
+		selectedCells.Add(other);
+	}
+
+	public void RemoveCells () {
+		foreach(Collider cell in selectedCells) {
+			cell.attachedRigidbody.useGravity = true;
+			cell.isTrigger = false;
+		}
+
+	}
+}
