b7dba5729d74091beaf36ad926c95df40c993019
KristianBerg <kristianberg.jobb@gmail.com>
2017-04-28 11:06:41 +0200

    Functionality for running R scripts along with config template and R installation instructions

---
+++ Assets/Scripts/RTest.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class RTest : MonoBehaviour {
+
+	// Demonstration of running a simple R script
+	void Start () {
+        string home = Directory.GetCurrentDirectory();
+        string rPath;
+        using (StreamReader r = new StreamReader(home + "/Assets/Config/config.txt"))
+        {
+            string input = r.ReadToEnd();
+            rPath = input;
+            Debug.Log(rPath);
+            Debug.Log(RScriptRunner.RunFromCmd(home + "/Assets/Scripts/test.R", rPath, ""));
+        }
+    }
+}
+
