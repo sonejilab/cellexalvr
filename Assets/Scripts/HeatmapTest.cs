dfe2de062d2ad751eb7b86adcd4b9ad680c7c384
KristianBerg <kristianberg.jobb@gmail.com>
2017-05-05 14:18:58 +0200

    You can now generate and display a heatmap from a hardcoded selection of cells, although it locks up the program

---
+++ Assets/Scripts/HeatmapTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class HeatmapTest : MonoBehaviour {
+    
+    public GameObject HeatmapImageBoard;
+    private ArrayList data;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("n"))
+        {
+            runHeatmap();
+            HeatmapImageBoard.GetComponent<ChangeImage>().updateImage("Assets/Images/heatmap.png");
+        }
+    }
+
+    public void runHeatmap()
+    {
+        string home = Directory.GetCurrentDirectory();
+        string rPath;
+        using (StreamReader r = new StreamReader(home + "/Assets/Config/config.txt"))
+        {
+            string input = r.ReadToEnd();
+            rPath = input;
+            Debug.Log("R result: " + RScriptRunner.RunFromCmd(home + "/Assets/Scripts/Make_heatmap_from_CellIS_list.R", rPath, ""));
+        }
+    }
+}
--- Assets/Scripts/Make_heatmap_from_CellIS_list.R
+++ Assets/Scripts/Make_heatmap_from_CellIS_list.R
@@ -1,9 +1,13 @@
+print("script started")
 library(gplots)
 library(pheatmap)
+print("libs loaded")
 
 make.heatmap.1grp <- function(dfile,cellidfile,outfile){
-
+  
+  print("func started")
   dat.pre <- read.delim(dfile,row.names=NULL,header=T,check.names=F)
+  print("data loaded")
   dat <- as.matrix(dat.pre[,-1])
   rownames(dat) <- dat.pre[,1]
 
@@ -23,10 +27,18 @@ make.heatmap.1grp <- function(dfile,cellidfile,outfile){
 
   #heatmap.2(dat.s,trace="none",scale="row",col=bluered(30))
 
+  print(outfile)
   png(outfile,height=800,width=1000)
+  print("created png")
   pheatmap(dat.f,cluster_rows=TRUE, show_rownames=FALSE,show_colnames=FALSE,cluster_cols=TRUE,scale="row",clustering_method = "ward.D2",col=bluered(16),breaks=seq(-4,4,by=0.5))
   dev.off()
+  print("almost done")
 }
 
-
-make.heatmap.1grp("Gene_Expression_Data.txt","CellIDs2heatmap_1GRP.txt","heatmap_out.png")
+print(getwd())
+setwd("C:/Users/Kristian/Documents/Unity_projects/vrJeans/Assets/Images")
+print(getwd())
+expression_data_filepath <- "C:/Users/Kristian/Documents/Unity_projects/vrJeans/Assets/Data/Gene_Expression_Data.txt"
+group_selection_filepath <- "C:/Users/Kristian/Documents/Unity_projects/vrJeans/Assets/Data/CellIDs2heatmap_1GRP.txt"
+make.heatmap.1grp(expression_data_filepath, group_selection_filepath,"heatmap.png")
+print("done")
