9b6eeedf16ff8449104430d2a40783d5c8b9c82b
KristianBerg <kristianberg.jobb@gmail.com>
2017-05-19 14:37:13 +0200

    reverted changes to R script and removed old version

--- Assets/Scripts/R/Make_heatmap_from_CellID_list_wColourBars_ANOVA.R
+++ Assets/Scripts/R/Make_heatmap_from_CellID_list_wColourBars_ANOVA.R
@@ -51,8 +51,8 @@ make.heatmap.multigrp.anova <- function(dfile,cellidfile,num.sig,outfile){
 
   #print(dim(dat.f[sigp,]))
 
-  png(outfile,height=8000,width=10000)
-  pheatmap(dat.f[sigp,],cluster_rows=TRUE, show_rownames=T,show_colnames=FALSE,cluster_cols=FALSE,scale="row",clustering_method = "ward.D2",col=bluered(16),breaks=seq(-4,4,by=0.5),annotation_col = annotation_col,annotation_colors=rcolrs)
+  png(outfile,height=800,width=1000)
+  pheatmap(dat.f[sigp,],cluster_rows=TRUE, show_rownames=F,show_colnames=FALSE,cluster_cols=FALSE,scale="row",clustering_method = "ward.D2",col=bluered(16),breaks=seq(-4,4,by=0.5),annotation_col = annotation_col,annotation_colors=rcolrs)
   dev.off()
 }
 
