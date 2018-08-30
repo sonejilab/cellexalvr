22b68e21ca2ff98e99b7ad10fc8c109c9904c999
JohanRodhe <fiv12jro@student.lu.se>
2018-08-30 14:00:07 +0200

    Video player. Videos not commited.

---
+++ Assets/Scripts/PlayVideo.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+[RequireComponent (typeof(AudioSource))]
+
+public class PlayVideo : MonoBehaviour {
+
+    //public MovieTexture movie;
+    public VideoClip videoClip;
+    public RawImage image;
+    private AudioSource audioSource;
+    private VideoSource videoSource;
+
+    private VideoPlayer videoPlayer;
+
+    // Use this for initialization
+    void Start()
+    {
+        Application.runInBackground = true;
+        StartCoroutine(playVideo());
+        //videoPlayer.Pause();
+    }
+
+    public void StartVideo()
+    {
+        if (!videoPlayer.isPlaying && videoPlayer.isPrepared)
+        {
+            videoPlayer.Play();
+            audioSource.Play();
+        }
+        if (!videoPlayer.isPlaying)
+        {
+            Application.runInBackground = true;
+            StartCoroutine(playVideo());
+        }
+        //videoPlayer.Pause();
+
+
+    }
+    public void PauseVideo()
+    {
+        videoPlayer.Pause();
+        audioSource.Pause();
+    }
+
+    public void StopVideo()
+    {
+        videoPlayer.Stop();
+        audioSource.Stop();
+    }
+
+    IEnumerator playVideo()
+    {
+
+        //Add VideoPlayer to the GameObject
+        videoPlayer = gameObject.AddComponent<VideoPlayer>();
+
+        //Add AudioSource
+        audioSource = gameObject.AddComponent<AudioSource>();
+
+        //Disable Play on Awake for both Video and Audio
+        videoPlayer.playOnAwake = false;
+        audioSource.playOnAwake = false;
+        audioSource.Pause();
+        
+
+        videoPlayer.source = VideoSource.VideoClip;
+
+
+        //Set Audio Output to AudioSource
+        videoPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
+
+        //Assign the Audio from Video to AudioSource to be played
+        videoPlayer.EnableAudioTrack(0, true);
+        videoPlayer.SetTargetAudioSource(0, audioSource);
+
+        //Set video To Play then prepare Audio to prevent Buffering
+        videoPlayer.clip = videoClip;
+        videoPlayer.Prepare();
+
+        //Wait until video is prepared
+        WaitForSeconds waitTime = new WaitForSeconds(1);
+        while (!videoPlayer.isPrepared)
+        {
+            Debug.Log("Preparing Video");
+            //Prepare/Wait for 5 sceonds only
+            yield return waitTime;
+            //Break out of the while loop after 5 seconds wait
+            break;
+        }
+
+        Debug.Log("Done Preparing Video");
+
+        //Assign the Texture from Video to RawImage to be displayed
+        image.texture = videoPlayer.texture;
+
+        //Play Video
+        //videoPlayer.Play();
+
+        //Play Sound
+        //audioSource.Play();
+
+        Debug.Log("Playing Video");
+        while (videoPlayer.isPlaying)
+        {
+            Debug.LogWarning("Video Time: " + Mathf.FloorToInt((float)videoPlayer.time));
+            yield return null;
+        }
+        Debug.Log("Done Playing Video");
+    }
+}
