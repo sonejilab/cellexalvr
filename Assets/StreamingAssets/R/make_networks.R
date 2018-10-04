1b6fd2616a03d4e3df9d561db17c61a2517cfa60
JohanRodhe <fiv12jro@student.lu.se>
2018-10-04 12:33:07 +0200

    arguments update

--- Assets/StreamingAssets/R/make_networks.R
+++ Assets/StreamingAssets/R/make_networks.R
@@ -1,36 +1,5 @@
 #print("start R")
 
-
-#library(cellexalvrR)
-
-#args <- commandArgs(trailingOnly = TRUE)
-
-#homedir <- args[1]
-
-#datadir <- args[2]
-#latest_version <- args[3]
-#input_file_dir <- args[4]
-
-#expression_data_filepath <- file.path(datadir, "cellexalObj.RData")
-
-#group_selection_filepath <- file.path(input_file_dir, paste("selection", latest_version, ".txt", sep=""))
-
-#if ( ! file.exists(group_selection_filepath)) {
-#        group_selection_filepath = latest_version # as this is the grouping name!
-#}
-
-#print(group_selection_filepath)
-
-#generated_table_filepath <- paste(file.path(homedir, "Resources","Networks"), "/", sep="")
-
-#cellexalvrObj <- make.cellexalvr.network(expression_data_filepath, group_selection_filepath, generated_table_filepath)
-
-#save ( cellexalvrObj , file = file.path(args[4], "cellexalvrObj.RData", sep="" ))
-
-
-#print("start R")
-
-
 library(cellexalvrR)
 
 args <- commandArgs(trailingOnly = TRUE)
