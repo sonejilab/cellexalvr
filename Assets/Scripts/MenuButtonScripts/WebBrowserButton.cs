d804f5c0af5eddb07f2b971214e2831796bacea9
JohanRodhe <fiv12jro@student.lu.se>
2018-10-15 10:47:09 +0200

    in game web browser + button to activate it|

---
+++ Assets/Scripts/MenuButtonScripts/WebBrowserButton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Toggle on/off the web browser tool. This also activates the laser on the right hand so that one can interact with the browser.
+/// </summary>
+public class WebBrowserButton : CellexalToolButton
+{
+    private void Start()
+    {
+        SetButtonActivated(true);
+    }
+
+    protected override string Description
+    {
+        get { return "Toggle Web Browser"; }
+    }
+
+
+    protected override ControllerModelSwitcher.Model ControllerModel
+    {
+        get { return ControllerModelSwitcher.Model.WebBrowser; }
+    }
+
+
+}
