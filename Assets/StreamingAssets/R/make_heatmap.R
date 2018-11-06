68ee2b91876afb6fecbb751b89779ac50563e7b0
Stefan Lang <Stefan.Lang@med.lu.se>
2018-11-06 10:54:44 +0100

    R scripts more verbose output.

--- Assets/StreamingAssets/R/make_heatmap.R
+++ Assets/StreamingAssets/R/make_heatmap.R
@@ -26,7 +26,11 @@ if ( ! file.exists(group_selection_filepath)) {
 
 #generated_image_filepath <- file.path(homedir, "Images", paste("heatmap_", latest_version, ".png", sep=""))
 
+message( paste("make_heatmap using grouping file", file.path(datadir, paste("selection", latest_version, ".txt", sep="") ) ) )
+
 cellexalvrObj <- make.cellexalvr.heatmap.list(expression_data_filepath,group_selection_filepath,top_genes_number,output_filepath)
+
+message( "Heatmap - no object save: grouping should already be stored" )
 
 #this is not necesary any more - done wile the group is stored.
 # lockedSave(cellexalObj)
