e4bf8f8f2f9aeb32d059c4565177dc9915fe5de9
JohanRodhe <fiv12jro@student.lu.se>
2018-11-14 09:21:47 +0100

    keyboard logic for web browser

---
+++ Assets/Scripts/KeyboardWebBrowser.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class KeyboardWebBrowser : CurvedVRKeyboard.KeyboardStatus {
+
+    public SimpleWebBrowser.WebBrowser webBrowser;
+
+    // Use this for initialization
+    void Start() {
+    }
+
+    protected override void BackspaceKey()
+    {
+        if (output.Length >= 1)
+        {
+            keyboardOutput.RemoveLetter();
+            output = output.Remove(output.Length - 1, 1);
+        }
+    }
+
+    protected override void SpaceKey()
+    {
+        TypeKey(' ');
+    }
+
+    protected override void TypeKey(char key)
+    {
+        if (output.Length < maxOutputLength)
+        {
+            keyboardOutput.AddLetter(key);
+            output = output + key.ToString();
+        }
+    }
+
+    public override void ClearKey()
+    {
+        base.ClearKey();
+        output = "";
+    }
+
+    public override void EnterKey()
+    {
+        print("Navigate to - " + output);
+        // If url field does not contain '.' then may not be a url so google the output instead
+        if (!output.Contains('.'))
+        {
+            output = "www.google.com/search?q=" + output;
+        }
+        webBrowser.OnNavigate(output);
+    }
+
+
+}
