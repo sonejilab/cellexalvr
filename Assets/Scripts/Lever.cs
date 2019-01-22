f48f00dce9bb5c6aced8396b1de5e84471a96d79
oscarlegetth <oscar.legetth@gmail.com>
2019-01-22 16:03:23 +0100

    lever

---
+++ Assets/Scripts/Lever.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    public ReferenceManager referenceManager;
+
+    private bool controllerInside = false;
+    private SteamVR_TrackedObject rightController;
+    private SteamVR_Controller.Device device;
+
+    private void Update()
+    {
+        device = SteamVR_Controller.Input((int)rightController.index);
+        if (controllerInside && device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            PullLever();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Controller"))
+        {
+            controllerInside = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Controller"))
+        {
+            controllerInside = false;
+        }
+    }
+
+    private void OnDisable()
+    {
+        controllerInside = false;
+    }
+
+    public void PullLever()
+    {
+        referenceManager.loaderController.LoadAllCells();
+    }
+}
