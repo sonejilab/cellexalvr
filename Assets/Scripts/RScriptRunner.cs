20386b0a4ede1df3aac00f333ce66584a77e42b7
JohanRodhe <fiv12jro@student.lu.se>
2019-02-26 14:00:06 +0100

    Remove spawn error from run from cmd

--- Assets/Scripts/RScriptRunner.cs
+++ Assets/Scripts/RScriptRunner.cs
@@ -59,7 +59,7 @@ public class RScriptRunner
         }
         catch (Exception ex)
         {
-            CellexalError.SpawnError("Error, R script failed.", string.Format("The R script at {0} failed with the message {1}", rCodeFilePath, ex.ToString()));
+            CellexalLog.Log("Error, R script failed.", string.Format("The R script at {0} failed with the message {1}", rCodeFilePath, ex.ToString()));
             throw new Exception("R Script failed: " + result, ex);
         }
     }
