e833d13eb33eddbfb028c6f6fc9aea8ec95374aa
JohanRodhe <fiv12jro@student.lu.se>
2019-05-28 09:32:28 +0200

    minor changes

--- Assets/Scripts/AnalysisLogic/SQLite.cs
+++ Assets/Scripts/AnalysisLogic/SQLite.cs
@@ -774,15 +774,23 @@ namespace SQLiter
         /// <param name="geneName"> The gene name </param>
         private IEnumerator QueryGeneCoroutine(string geneName, GraphManager.GeneExpressionColoringMethods coloringMethod)
         {
+            System.Diagnostics.Stopwatch stopWatch = new System.Diagnostics.Stopwatch();
+            stopWatch.Start();
             //int statusId = status.AddStatus("Querying database for gene " + geneName);
             _result.Clear();
-            string query = "select cname, value from datavalues left join cells on datavalues.cell_id = cells.id where gene_id = (select id from genes where upper(gname) = upper(\"" + geneName + "\"))";
+            string query = "select cname, value from datavalues left join cells on datavalues.cell_id = cells.id "+
+                "where gene_id = (select id from genes where upper(gname) = upper(\"" + geneName + "\"))";
             Thread t = new Thread(() => QueryThread(query));
             t.Start();
             while (t.IsAlive)
             {
                 yield return null;
             }
+            stopWatch.Stop();
+            print(stopWatch.Elapsed);
+
+            stopWatch.Reset();
+            stopWatch.Start();
 
             int i = 0;
             LowestExpression = float.MaxValue;
@@ -853,11 +861,14 @@ namespace SQLiter
                 }
                 _result.AddRange(result);
             }
+            stopWatch.Stop();
+            print(stopWatch.Elapsed);
             if (DebugMode)
                 print("Number of columns returned from database: " + i);
             _reader.Close();
             _connection.Close();
             //status.RemoveStatus(statusId);
+
             QueryRunning = false;
         }
 
