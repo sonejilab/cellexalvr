775dfd24a7b5e433fbc44360ed7884e44c20cad6
JohanRodhe <fiv12jro@student.lu.se>
2019-11-05 15:58:57 +0100

    move button editor code to button base class

--- Assets/Scripts/Menu/Buttons/CellexalButton.cs
+++ Assets/Scripts/Menu/Buttons/CellexalButton.cs
@@ -3,6 +3,7 @@ using VRTK;
 using CellexalVR.General;
 using CellexalVR.Menu.Buttons.General;
 using CellexalVR.Menu.SubMenus;
+using UnityEditor;
 
 namespace CellexalVR.Menu.Buttons
 {
@@ -299,5 +300,42 @@ namespace CellexalVR.Menu.Buttons
             }
             controllerInside = highlight;
         }
+    }
+
+    /// <summary>
+    /// A custom inspector for all buttons to help show only fields that are used.
+    /// </summary>
+    [CustomEditor(typeof(CellexalButton), editorForChildClasses: true)]
+    [CanEditMultipleObjects]
+    public class CellexalButtonEditor : Editor
+    {
+        public string[] buttonTypeOptions = new string[] { "Mesh", "Sprite" };
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            var buttonScript = target as CellexalButton;
+            buttonScript.popupChoice = EditorGUILayout.Popup("Button Type", buttonScript.popupChoice, buttonTypeOptions, EditorStyles.popup);
+            if (buttonScript.popupChoice == 0)
+            {
+                //EditorGUILayout.PrefixLabel("Mesh options");
+                buttonScript.meshStandardColor = EditorGUILayout.ColorField("Standard Color", buttonScript.meshStandardColor);
+                buttonScript.meshHighlightColor = EditorGUILayout.ColorField("Highlighted Color", buttonScript.meshHighlightColor);
+                buttonScript.meshDeactivatedColor = EditorGUILayout.ColorField("Deactivated Color", buttonScript.meshDeactivatedColor);
+
+            }
+            else if (buttonScript.popupChoice == 1)
+            {
+                //EditorGUILayout.PrefixLabel("Sprite options");
+                buttonScript.standardTexture = (UnityEngine.Sprite)EditorGUILayout.ObjectField("Standard texture", buttonScript.standardTexture, typeof(UnityEngine.Sprite), true);
+                buttonScript.highlightedTexture = (UnityEngine.Sprite)EditorGUILayout.ObjectField("Highlighted texture", buttonScript.highlightedTexture, typeof(UnityEngine.Sprite), true);
+                buttonScript.deactivatedTexture = (UnityEngine.Sprite)EditorGUILayout.ObjectField("Deactivated texture", buttonScript.deactivatedTexture, typeof(UnityEngine.Sprite), true);
+            }
+            EditorUtility.SetDirty(buttonScript);
+            base.OnInspectorGUI();
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
     }
 }
