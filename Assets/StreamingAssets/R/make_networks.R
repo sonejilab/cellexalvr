90297eeab10e9dd7ee8ee3a9195924b331bbe2d4
JohanRodhe <fiv12jro@student.lu.se>
2019-11-18 15:41:46 +0100

    network script minor change

--- Assets/StreamingAssets/R/make_networks.R
+++ Assets/StreamingAssets/R/make_networks.R
@@ -13,7 +13,7 @@ network_method <- args[4] # the algorithm to use
 expression_data_filepath <- file.path(datadir, "cellexalObj.RData")
 
 # the script that will be run by the r session. Needs to be on the correct format to be read properly by the r source command. Change this line if you want to run your own network function.
-function_str <- paste("make.cellexalvr.network(cellexalObj,
+function_str <- paste("cellexalObj = make.cellexalvr.network(cellexalObj,
  			\"", group_selection_filepath, "\",
 			\"", output_filepath, "\",
 			\"method=", network_method , "\")",
