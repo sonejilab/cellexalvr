b36c66d0b63b7553546faec9879a798dc859cbfd
JohanRodhe <fiv12jro@student.lu.se>
2020-01-14 16:45:43 +0100

    m

--- Assets/Scripts/AnalysisLogic/h5reader/h5readerAnnotater.cs
+++ Assets/Scripts/AnalysisLogic/h5reader/h5readerAnnotater.cs
@@ -33,7 +33,7 @@ public class h5readerAnnotater : MonoBehaviour
         startInfo.RedirectStandardError = true;
         startInfo.WindowStyle = ProcessWindowStyle.Minimized;
 
-        startInfo.FileName = "py.exe";
+        startInfo.FileName = "python";
 
         string file_name = "LCA_142K_umap_phate.loom";
         startInfo.Arguments = "crawl.py " + "Data/" + file_name;
@@ -61,11 +61,6 @@ public class h5readerAnnotater : MonoBehaviour
         display.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, 0, contentSize);
     }
 
-    public static void testPrint()
-    {
-        print("test");
-    }
-
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.M))
@@ -73,95 +68,4 @@ public class h5readerAnnotater : MonoBehaviour
             keys.updatePosition(10f);
         }
     }
-
-    //class KeyHolder : MonoBehaviour
-    //{
-    //    public Dictionary<string,KeyHolder> subkeys = new Dictionary<string, KeyHolder>();
-    //    public KeyHolder parent;
-    //    public string name;
-    //    public GameObject go;
-    //    public RectTransform rect;
-    //    public TextMeshProUGUI tmp;
-    //    public BoxCollider boxCollider;
-
-    //    public KeyHolder(string name)
-    //    {
-    //        this.name = name;
-    //    }
-
-    //    public void insert(string name)
-    //    {
-    //        if (name.Contains("/"))
-    //        {
-    //            string parentKey = name.Substring(0, name.IndexOf("/"));
-    //            string newName = name.Substring(name.IndexOf("/") + 1);
-    //            if(!subkeys.ContainsKey(parentKey))
-    //                subkeys.Add(parentKey, new KeyHolder(parentKey));
-    //            subkeys[parentKey].insert(newName);
-    //        }
-    //        else
-    //        {
-    //            subkeys.Add(name, new KeyHolder(name));
-    //        }
-    //    }
-
-    //    public string outputPrint(string push = "")
-    //    {
-    //        string ret = push + name + "\n";
-    //        foreach (KeyHolder k in subkeys.Values)
-    //            ret+=k.outputPrint(push + "|--");
-    //        return ret;
-    //    }
-
-    //    public void fillContent(RectTransform content, int depth = 0)
-    //    {
-    //        go = new GameObject(name);
-    //        go.transform.SetParent(content);
-    //        rect = go.AddComponent<RectTransform>();
-    //        go.AddComponent<CanvasRenderer>();
-    //        tmp = go.AddComponent<TextMeshProUGUI>();
-    //        boxCollider = go.AddComponent<BoxCollider>();
-    //        boxCollider.isTrigger = true;
-
-    //        rect.localScale = new Vector3(1, 1, 1);
-    //        rect.anchorMin = new Vector2(0, 1);
-    //        rect.anchorMax = new Vector2(1, 0);
-    //        rect.pivot = new Vector2(0.5f, 1);
-    //        rect.localPosition = Vector3.zero;
-    //        rect.localEulerAngles = Vector3.zero;
-
-    //        tmp.fontSize = 8;
-    //        tmp.alignment = TextAlignmentOptions.MidlineLeft;
-    //        string text = "";
-    //        for (int i = 0; i < depth; i++)
-    //        {
-    //            text += "--";
-    //        }
-    //        text += "> ";
-    //        tmp.text = text + name;
-
-    //        foreach (KeyHolder k in subkeys.Values)
-    //        {
-    //            k.fillContent(rect, depth + 1);
-    //        }
-    //    }
-
-    //    public float updatePosition(float offset = 0f)
-    //    {
-    //        rect.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, offset, 10f);
-    //        rect.sizeDelta = new Vector2(0,rect.sizeDelta.y);
-    //        boxCollider.center = new Vector3(0, -5,0);
-    //        boxCollider.size = new Vector3(160, 10,1);
-    //        float temp = 0f;
-    //        foreach (KeyHolder k in subkeys.Values)
-    //        {
-    //            if (k.go.activeSelf)
-    //            {
-    //                temp += 10f;
-    //                temp += k.updatePosition(temp);
-    //            }
-    //        }
-    //        return temp;
-    //    }
-    //}
 }
