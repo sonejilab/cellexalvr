15e868e5c339c216ae7f2e58fc9226fe617d93c4
JohanRodhe <fiv12jro@student.lu.se>
2020-01-20 13:20:59 +0100

    m

---
+++ Assets/Scripts/AnalysisLogic/h5reader/h5sphereScript.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class h5sphereScript : MonoBehaviour
+{
+    public Color color;
+    public string Type;
+}
