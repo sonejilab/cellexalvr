2010156ebe340dd387023b6d0c428cd364e7c2b8
JohanRodhe <fiv12jro@student.lu.se>
2020-02-19 10:59:53 +0100

    page handling in menu with tabs

---
+++ Assets/Scripts/Menu/SubMenus/ChangePageButton.cs
@@ -0,0 +1,27 @@
+﻿using CellexalVR.Menu.Buttons;
+using CellexalVR.Menu.SubMenus;
+using System;
+
+namespace Assets.Scripts.Menu.SubMenus
+{
+
+    public class ChangePageButton : CellexalButton
+    {
+        public int dir;
+        private AttributeSubMenu attributeSubMenu;
+        protected override string Description
+        {
+            get { return " "; }
+        }
+
+        private void Start()
+        {
+            attributeSubMenu = GetComponentInParent<AttributeSubMenu>();
+        }
+
+        public override void Click()
+        {
+            attributeSubMenu.ChangePage(dir);
+        }
+    }
+}
--- Assets/Scripts/Menu/SubMenus/MenuWithTabs.cs
+++ Assets/Scripts/Menu/SubMenus/MenuWithTabs.cs
@@ -13,13 +13,16 @@ namespace CellexalVR.Menu.SubMenus
     {
         public ReferenceManager referenceManager;
         public Tab tabPrefab;
+        public GameObject nextPageButton;
+        public GameObject prevPageButton;
+        public TextMeshPro pageNrText;
         //public Dictionary<CellexalButton, bool> savedButtonStates = new Dictionary<CellexalButton, bool>();
 
         protected MenuToggler menuToggler;
         protected List<Tab> tabs = new List<Tab>();
-        protected Vector3 tabButtonPos = new Vector3(-0.367f, 1f, 0.325f);
-        protected Vector3 tabButtonPosOriginal = new Vector3(-0.367f, 1f, 0.325f);
-        protected Vector3 tabButtonPosInc = new Vector3(0.25f, 0, 0);
+        protected Vector3 tabButtonPos = new Vector3(-0.309f, 1f, 0.325f);
+        protected Vector3 tabButtonPosOriginal = new Vector3(-0.309f, 1f, 0.325f);
+        protected Vector3 tabButtonPosInc = new Vector3(0.2f, 0, 0);
 
         public CellexalButton prefab;
 
@@ -29,6 +32,9 @@ namespace CellexalVR.Menu.SubMenus
         protected int buttonsPerTab = 20;
         protected string[] names;
         protected string[] categories;
+
+        private int currentPage = 0;
+        private int pageCounter = 0;
 
         private void OnValidate()
         {
@@ -97,6 +103,13 @@ namespace CellexalVR.Menu.SubMenus
                 // add a new tab if we encounter a new category, or if the current tab is full
                 if (buttonIndex % buttonsPerTab == 0 || i > 0 && categories[i] != categories[i - 1])
                 {
+                    if (tabs.Count > 0 && tabs.Count % 4 == 0)
+                    {
+                        nextPageButton.SetActive(true);
+                        tabButtonPos = tabButtonPosOriginal;
+                        pageCounter++;
+                        pageNrText.text = "p. " + (currentPage + 1) + "/" + (pageCounter + 1);
+                    }
                     newTab = AddTab(tabPrefab);
                     newTab.TabButton.GetComponentInChildren<TextMeshPro>().text = categories[i];
                     buttonIndex = 0;
@@ -136,7 +149,14 @@ namespace CellexalVR.Menu.SubMenus
         public virtual T AddTab<T>(T tabPrefab) where T : Tab
         {
             var newTab = Instantiate(tabPrefab, transform);
-            newTab.gameObject.SetActive(true);
+            if (tabs.Count >= 4)
+            {
+                newTab.gameObject.SetActive(false);
+            }
+            else
+            {
+                newTab.gameObject.SetActive(true);
+            }
             //newTab.SetTabActive(false);
             //newTab.transform.parent = transform;
             newTab.TabButton.gameObject.transform.localPosition = tabButtonPos;
@@ -205,6 +225,30 @@ namespace CellexalVR.Menu.SubMenus
             {
                 tab.SetTabActive(false);
             }
+        }
+
+        public void ChangePage(int dir)
+        {
+            currentPage += dir;
+            pageNrText.text = "p. " + (currentPage + 1) + "/" + (pageCounter + 1);
+            for (int i = 0; i < tabs.Count; i++)
+            {
+                tabs[i].gameObject.SetActive((int)(i / 4) == currentPage);
+            }
+            if (currentPage == pageCounter)
+            {
+                nextPageButton.SetActive(false);
+            }
+            else if (currentPage == 0)
+            {
+                prevPageButton.SetActive(false);
+            }
+            else
+            {
+                nextPageButton.SetActive(true);
+                prevPageButton.SetActive(true);
+            }
+
         }
     }
 }
