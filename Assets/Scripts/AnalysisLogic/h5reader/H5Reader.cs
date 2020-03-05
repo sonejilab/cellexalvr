cff26c6f947990c753e8e6f64f132d553f77bf97
JohanRodhe <fiv12jro@student.lu.se>
2020-03-05 10:56:49 +0100

    starting python process in new thread

--- Assets/Scripts/AnalysisLogic/h5reader/H5Reader.cs
+++ Assets/Scripts/AnalysisLogic/h5reader/H5Reader.cs
@@ -79,7 +79,6 @@ namespace CellexalVR.AnalysisLogic.H5reader
                     filePath = s;
             }
 
-
             if (configFile == "")
             {
                 UnityEngine.Debug.Log("No config file for " + path);
@@ -171,8 +170,14 @@ namespace CellexalVR.AnalysisLogic.H5reader
             string file_name = filePath;
             startInfo.Arguments = "ann.py " + file_name;
             p.StartInfo = startInfo;
-            p.Start();
-
+            Thread t = new Thread(
+                () =>
+                {
+                    bool start = p.Start();
+                });
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            t.Start();
 
             writer = p.StandardInput;
 
@@ -477,7 +482,7 @@ namespace CellexalVR.AnalysisLogic.H5reader
                     }
                 }
 
-                if (HighestExpression == LowestExpression)
+                if (Math.Abs(HighestExpression - LowestExpression) < 0.001)
                 {
                     HighestExpression += 1;
                 }
