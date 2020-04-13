bc8ba4918598d20a72fe3a723981964572efc467
heheh111 <dat14eto@student.lu.se>
2020-04-13 15:13:54 +0200

    m

--- Assets/Scripts/AnalysisLogic/h5reader/H5ReaderAnnotatorScriptManager.cs
+++ Assets/Scripts/AnalysisLogic/h5reader/H5ReaderAnnotatorScriptManager.cs
@@ -9,6 +9,9 @@ namespace CellexalVR.AnalysisLogic.H5reader
         public GameObject annotatorPrefab;
         public Dictionary<string, H5readerAnnotater> annotators = new Dictionary<string, H5readerAnnotater>();
         private ReferenceManager referenceManager;
+
+        private Vector3 finalPoint;
+        private Vector3 finalScale;
         // Start is called before the first frame update
         private void Start()
         {
@@ -28,7 +31,21 @@ namespace CellexalVR.AnalysisLogic.H5reader
             {
                 GameObject go = Instantiate(annotatorPrefab, this.transform);
                 go.transform.parent = transform;
-                go.transform.localPosition = Vector3.zero;
+                go.transform.localRotation = Quaternion.Euler(0, -90, 0);
+                finalScale = go.transform.localScale;
+                go.transform.localScale *= 0.01f;
+
+
+                Transform funnel = referenceManager.loaderController.transform;
+                print(funnel.transform.position);
+
+                go.transform.position = funnel.transform.position;
+
+                finalPoint = funnel.transform.position + Vector3.up*1.2f;
+                print(finalPoint);
+
+                StartCoroutine(SpawnAnimation(go));
+
                 H5readerAnnotater script = go.GetComponent<H5readerAnnotater>();
                 script.Init(path);
                 annotators.Add(path, script);
@@ -38,6 +55,35 @@ namespace CellexalVR.AnalysisLogic.H5reader
         public void RemoveAnnotator(string path)
         {
             annotators[path].gameObject.SetActive(false);
+        }
+
+        IEnumerator SpawnAnimation(GameObject go)
+        {
+            // Getting the difference in position and rotation
+            Vector3 deltaPos = finalPoint - go.transform.position;
+            Vector3 deltaScale = finalScale - go.transform.localScale;
+            //Vector3 deltaRotation = finalPoint.rotation.eulerAngles - transform.rotation.eulerAngles;
+            // Repeat once per frame for a second:
+            yield return new WaitForSeconds(1f);
+            float timePassed = 0;
+            while (timePassed < 1.0f)
+            {
+                print(deltaPos);
+                // Increment the time to prevent an infinite loop
+                timePassed += Time.deltaTime;
+                // Slowly adjust the position and rotation so it approaches the spawn position and rotation
+                go.transform.position += deltaPos * Time.deltaTime;
+                go.transform.localScale += deltaScale * Time.deltaTime;
+                //transform.Rotate(deltaRotation * Time.deltaTime);
+                // Exit the function (it will begin where it left off the next frame)
+                yield return null;
+            }
+            // Ensures the player is exactly where you want him when the time is up           
+            go.transform.position = finalPoint;
+            go.transform.localScale = finalScale;
+            //transform.rotation = spawnPoint.rotation;
+            // Waits for a second after the player has returned before running the game again
+            //yield return new WaitForSeconds(1f);
         }
     }
 
